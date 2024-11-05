9ea757ee6777629adfc00f285cc527406c96e6f2
Rizal Purnawan <rizalpurnawan23@gmail.com>
2024-11-06 05:03:40 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DAMSMEL',
-    version='0.0.1',
+    version='0.1.0-beta',
     packages=find_packages(),
     description="A continuous optimisation algorithm",
     author="Rizal Purnawan and Dieky Adzkiya",
